aed7eccd181606dea417224f245816c86c088b82
Анастасия Слуцких <anastasuasluckix94@gmail.com>
2022-03-27 22:59:53 +0700

    Добавьте файлы проекта.

---
+++ Lesson5_Method Words/Lesson5_ Rearrangement of words/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5__Rearrangement_of_words
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Разделяет введенное предложение из консоли на массив слов
+        /// </summary>
+        /// <param name="pred"></param>
+        /// <returns></returns>
+        static string[] SplittingStringWords(string pred)
+        {
+
+
+            string[] Words = pred.Split(' ');
+
+
+
+            return Words;
+        }
+        /// <summary>
+        /// Ввывод введенного предложение из консоли в обратном порядке
+        /// </summary>
+        /// <param name="str"></param>
+        static void ReversWords (string str)
+
+        {
+
+            string[] ResultString = SplittingStringWords (str);
+
+            string NewString= " ";
+             for (int i = ResultString.Length; i >0; i--)
+                  NewString += ResultString[i-1]+ " ";
+               
+              
+
+            Console.Write($"Новое предложение:{NewString}");
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string Predlogenie = Console.ReadLine();
+            ReversWords(Predlogenie);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson5_Method Words/Lesson5_Method Words/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_Method_Words
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Получает введеное предложение с консоли и разбивает его на слова
+        /// </summary>
+        /// <param name="pred"></param>
+        /// <returns></returns>
+        static string[] SplittingStringWords(string pred)
+        {
+           
+          
+            string[] Words = pred.Split(' ');
+
+            Print(Words);
+
+            return Words;
+        }
+        /// <summary>
+        /// Ввыодит на консоль слова
+        /// </summary>
+        /// <param name="per"></param>
+        static void Print(string[] per)
+        {
+            foreach (string p in per)
+                Console.WriteLine($"{p}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string Predlogenie = Console.ReadLine();
+            SplittingStringWords(Predlogenie);
+
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
